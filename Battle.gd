2135a26826d11dd2e4a9444a149ebe219a982def
Aramis Diaz <aramisdiaz@mail.usf.edu>
2020-10-02 15:49:41 -0700

    Initial templates

---
+++ Battle.gd
@@ -0,0 +1,60 @@
+extends Node
+
+const BattleUnits = preload("res://BattleUnits.tres")
+
+export(Array, PackedScene) var enemies = []
+
+onready var battleActionButtons = $UI/BattleActionButtons
+onready var animationPlayer = $AnimationPlayer
+onready var nextRoomButton = $UI/CenterContainer/NextRoomButton
+onready var enemyPosition = $EnemyPosition
+onready var textbox = $UI/TextBoxPanel/Textbox
+
+func _ready():
+	randomize()
+	start_player_turn()
+	var enemy = BattleUnits.Enemy
+	if enemy != null:
+		enemy.connect("died", self, "_on_Enemy_died")
+		enemy.connect("victor", self, "_on_Enemy_victor")
+
+
+func start_enemy_turn():
+	battleActionButtons.hide()
+	var enemy = BattleUnits.Enemy
+	if enemy != null and not enemy.is_queued_for_deletion():
+		enemy.attack()
+		yield(enemy, "end_turn")
+	start_player_turn()
+
+func start_player_turn():
+	battleActionButtons.show()
+	var playerStats = BattleUnits.PlayerStats
+	playerStats.ap = playerStats.max_ap
+	yield(playerStats, "end_turn")
+	start_enemy_turn()
+
+func create_new_enemy():
+	enemies.shuffle()
+	var Enemy = enemies.front()
+	var enemy = Enemy.instance()
+	enemyPosition.add_child(enemy)
+	enemy.connect("died", self, "_on_Enemy_died")
+	enemy.connect("victor", self, "_on_Enemy_victor")
+	
+func _on_Enemy_died():
+	nextRoomButton.show()
+	battleActionButtons.hide()
+
+func _on_Enemy_victor():
+	battleActionButtons.hide()
+	textbox.text = "GAME OVER"
+
+func _on_NextRoomButton_pressed():
+	nextRoomButton.hide()
+	animationPlayer.play("FadeToNewRoom")
+	yield(animationPlayer, "animation_finished")
+	var playerStats = BattleUnits.PlayerStats
+	playerStats.ap = playerStats.max_ap
+	battleActionButtons.show()
+	create_new_enemy()
